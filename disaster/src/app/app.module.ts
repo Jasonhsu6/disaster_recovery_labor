4125771a16338060f156536e2b5b312fba8c4f8b
Jasonhsu6 <35356776+Jasonhsu6@users.noreply.github.com>
2019-11-04 14:12:10 -0500

    index and joblist component

--- disaster/src/app/app.module.ts
+++ disaster/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
