2ca6e285bba563cfcd88d08779ecae739cd8faac
unknown <jasonnelson11@gmail.com>
2019-11-04 14:14:44 -0500

    Added Cors, node server is done

--- labor_management/server.js
+++ labor_management/server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var PORT = 3000;
 var url = "mongodb://jasonnelson:jasonnelson1@ds141168.mlab.com:41168/heroku_b67cn7n5"
@@ -8,12 +7,13 @@ const jobs = require("./Routes/jobs");
 const machines = require("./Routes/machines");
 const timecards = require("./Routes/timecards");
 const users = require("./Routes/UserRoutes");
+const cors = require("cors")
 
 app.use("/users",users)
 app.use('/machines', machines);
 app.use('/timecards', timecards);
 app.use('/jobs', jobs);
-
+app.use(cors)
 
 mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true} ,  function(err){
     if(err){
