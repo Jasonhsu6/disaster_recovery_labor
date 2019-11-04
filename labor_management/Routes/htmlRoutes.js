4da43bb15078f6d2aecd86a440d28a5d197141e9
Jasonhsu6 <35356776+Jasonhsu6@users.noreply.github.com>
2019-11-04 10:53:19 -0500

    routes

--- labor_management/Routes/htmlRoutes.js
+++ labor_management/Routes/htmlRoutes.js
@@ -1,27 +1,27 @@
-var db = require("../Schemas/index");
-var hash = require('object-hash');
-const fs   = require('fs');
-const jwt  = require('jsonwebtoken');
+// var db = require("../Schemas/index");
+// var hash = require('object-hash');
+// const fs   = require('fs');
+// const jwt  = require('jsonwebtoken');
 
-function hasher(password){
- return hash({Password:password})
-}
-function genHashCode(username,hash_password){
-jwt.sign({
-    Username:username,
-    Password:hash_password
-},"secret",{
-    expiresIn = "12h"
-})
-}
+// function hasher(password){
+//  return hash({Password:password})
+// }
+// function genHashCode(username,hash_password){
+// jwt.sign({
+//     Username:username,
+//     Password:hash_password
+// },"secret",{
+//     expiresIn = "12h"
+// })
+// }
 
-module.exports = function(app){
-    app.get("/",function(req,res){
-        res.send("Welcome to the first page")
-    })
-    app.post("/SignUp",function(req,res){
-        var username = req.body.Username;
-        var password = req.body.Password;
+// module.exports = function(app){
+//     app.get("/",function(req,res){
+//         res.send("Welcome to the first page")
+//     })
+//     app.post("/SignUp",function(req,res){
+//         var username = req.body.Username;
+//         var password = req.body.Password;
 
-    })
-}
+//     })
+// }
--- labor_management/server.js
+++ labor_management/server.js
@@ -4,12 +4,19 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var PORT = 3000;
 var url = "mongodb://jasonnelson:jasonnelson1@ds141168.mlab.com:41168/heroku_b67cn7n5"
+const jobs = require("./Routes/jobs");
+const machines = require("./Routes/machines");
+const timecards = require("./Routes/timecards");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-require("./Routes/htmlRoutes")(app);
+app.use('/machines', machines);
+app.use('/timecards/', timecards);
+app.use('/jobs/', jobs);
+
+// require("./Routes/htmlRoutes")(app);
 
 mongoose.connect(url,function(err){
     if(err){
@@ -18,7 +25,7 @@ mongoose.connect(url,function(err){
     else{
         console.log("Database Connected")
     }
-    })
+})
 
 app.listen(PORT,function(){
     console.log("Listening on port " + PORT)
